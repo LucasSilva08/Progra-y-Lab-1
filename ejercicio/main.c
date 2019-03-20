07c9e798202778370cd6c1b46ee37c9f2a795c95
Lucas Silva <lucasnicolas.silva@hotmail.com.ar>
2019-03-19 21:52:37 -0300

    ejercicio terminado

--- ejercicio/main.c
+++ ejercicio/main.c
@@ -10,13 +10,14 @@ int main()
     int sumaDePositivos=0;
     int contadorNegativos=0;
     int acumuladorNegativos=0;
-    float PromedioNegativos;
+    float promedioNegativos;
     int maximo;
     int minimo;
     int flag=0;
     int positivoMinimo;
     int negativoMaximo;
-    int flag1=0;
+    int flagPositivos=0;
+    int flagNegativo=0;
     int contadorDos=0;
     float promedioTotal;
     char respuesta='s';
@@ -30,11 +31,73 @@ int main()
             printf("Error. Ingrese un Numero Distinto a 0:");
             scanf("%d",&numero);
         }
+
+        acumuladorTotal=acumuladorTotal+numero;
+        contador++;
+
+        if (numero%2==0)
+        {
+            contadorPares++;
+        }
+        if (numero>0)
+        {
+            sumaDePositivos=sumaDePositivos+numero;
+            if(flagPositivos==0||numero<positivoMinimo)
+            {
+                positivoMinimo=numero;
+                flagPositivos=1;
+            }
+
+        }
+        else
+        {
+            acumuladorNegativos=acumuladorNegativos+numero;
+            contadorNegativos++;
+            if(flagNegativo==0||numero>negativoMaximo)
+            {
+                negativoMaximo=numero;
+                flagNegativo=1;
+            }
+        }
+
+        if(flag==0||numero>maximo)
+        {
+            maximo=numero;
+        }
+        if(flag==0||numero<minimo)
+        {
+            minimo=numero;
+            flag=1;
+        }
+        if (numero>=-10&&numero<=50)
+        {
+            contadorDos++;
+        }
+
+
         printf("Desea Continuar s/n: ");
-        fflush(stdin);
+        setbuf(stdin,NULL);
         scanf("%c",&respuesta);
+        getchar();
     }
 
+    promedioTotal=(float)acumuladorTotal/contador;
+
+    printf("La cantidad de Numeros ingresados es: %d\nLa cantidad de Pares es: %d\nLa Suma de los Positivos es: %d\n",contador,contadorPares,sumaDePositivos);
+
+    if(contadorNegativos!=0)
+    {
+        promedioNegativos=(float)acumuladorNegativos/contadorNegativos;
+        printf("El promedio de los Negativos es: %.2f\n",promedioNegativos);
+
+    }
+    else
+    {
+        printf("No se Ingresaron Negativos\n");
+    }
+
+    printf("El maximo es: %d\nEl minimo es: %d\nEl menor Positivo: %d\nEl maximo Negativo: %d\n",maximo,minimo,positivoMinimo,negativoMaximo);
+    printf("Cantidad de Numeros entre -10 y 50: %d\nEl promedio Total: %.2f\n",contadorDos,promedioTotal);
 
 
 
@@ -45,10 +108,12 @@ int main()
 
 
 
+    return 0;
 
 
 
-   /* int numero1;
+
+    /*int numero1;
     int numero2;
     int suma;
     float promedio;
@@ -63,5 +128,5 @@ int main()
     promedio=(float)suma/2;
 
     printf("El promedio es: %.1f",promedio);*/
-    return 0;
+
 }
