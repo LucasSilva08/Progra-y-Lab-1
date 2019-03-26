404b31ae2300fd6bfb2546c56461f8eebb636675
lucas <lucasnicolas.silva@hotmail.com.ar>
2019-03-26 10:35:06 -0300

    clase bibliotecas

---
+++ clase3/ingresos.h
@@ -0,0 +1,12 @@
+
+
+
+/** \brief le pide al usuario el ingreso de una numero
+ *
+ * \param char[] El mensaje a ser mostrado
+ * \return int el numero ingresado
+ *
+ */
+int pedirEntero(char[]);
+
+//prototipos de funciones a utilizar
---
+++ clase3/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ingresos.h"
+
+int main()
+{
+    int resultado = factorial(5);
+
+    printf("%d",resultado);
+
+    return 0;
+}
+
+/*int main()
+{
+    int legajo;
+    int edad;
+    int peso;
+    edad=pedirEntero("ingrese Edad: ");
+    legajo=pedirEntero("Ingrese Legajo: ");
+    peso=pedirEntero("ingrese peso: ");
+
+    return 0;
+}*/
