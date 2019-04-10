b7b0894763ef4249bcf6685b70c987e3a09e7492
Lucas Silva <lucasnicolas.silva@hotmail.com.ar>
2019-04-10 20:10:33 -0300

    Mayusculas y Minusculas

---
+++ Mayus-minus/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "MayMin.h"
+#include <string.h>
+
+int main()
+{
+    char cadena[20];
+
+    printf("Ingrese Nombre: ");
+    setbuf(stdin,NULL);
+    scanf("%[^\n]",cadena);
+
+    pasarMayuscula(cadena);
+    puts(cadena);
+
+    return 0;
+}
